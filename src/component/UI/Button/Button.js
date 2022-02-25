cacf3fa39441f6f14dc08c051879586e023ac669
Soumyak1303 <soumyak1303@gmail.com>
2022-02-25 12:23:08 +0530

    css fixes

--- src/component/UI/Button/Button.js
+++ src/component/UI/Button/Button.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./Button.css";
 
 const button = (props) => (
-  <button
-    disabled={props.disabled}
-    className={["Button form-btn", `${props.btnType}`].join(" ")}
-    onClick={props.clicked}
-  >
-    {props.children}
-  </button>
+  <div className={`btn-container ${props.disabled ? "disabled" : ""}`}>
+    <button
+      disabled={props.disabled}
+      className={["Button form-btn", `${props.btnType}`].join(" ")}
+      onClick={props.clicked}
+    >
+      {props.children}
+    </button>
+    <div className="tooltip">
+      <i>i</i>
+      <span>Please complete steps 1-3 to proceed.</span>
+    </div>
+  </div>
 );
 
 export default button;
